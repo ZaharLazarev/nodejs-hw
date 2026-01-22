3026891c1f3b26aaedc938487bac88d5acea9877
Zakhar <zaharlazarev123@gmail.com>
2026-01-23 01:46:08 +0200

    second_Assign

--- src/server.js
+++ src/server.js
@@ -25,7 +25,7 @@ const startServer = async () => {
   await connectMongoDB();
 
   app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on https://nodejs-hw-2-7zkx.onrender.com`);
   });
 };
 
