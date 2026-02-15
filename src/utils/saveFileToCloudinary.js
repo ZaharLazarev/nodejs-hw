1a90ad0c44c7a219be67e523f584d49979585f6e
Zakhar <zaharlazarev123@gmail.com>
2026-02-15 17:10:45 +0200

    fourth_assign

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -2,14 +2,14 @@ import { v2 as cloudinary } from 'cloudinary';
 import { Readable } from 'stream';
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-export const saveFileToCloudinary = (buffer) => {
+export const saveFileToCloudinary = async (buffer) => {
   return new Promise((resolve, reject) => {
-    const stream = cloudinary.uploader.upload_stream(
+    const uploadStream = cloudinary.uploader.upload_stream(
       {
         folder: 'avatars',
         resource_type: 'image',
@@ -17,11 +17,15 @@ export const saveFileToCloudinary = (buffer) => {
         unique_filename: true,
       },
       (error, result) => {
-        if (error) reject(error);
-        else resolve(result);
+        if (error) {
+          reject(error);
+          return;
+        }
+
+        resolve(result);
       },
     );
 
-    Readable.from(buffer).pipe(stream);
+    Readable.from(buffer).pipe(uploadStream);
   });
 };
